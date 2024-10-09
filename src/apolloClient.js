4bdc6323af286f35abca7d8cd09f2120fa722649
ayman-awal <ayman.awal36@gmail.com>
2024-10-08 20:28:24 -0500

    Fix small issue in apollo client setup

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,6 +1,8 @@
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
     uri: 'http://localhost:5000/graphql',
     cache: new InMemoryCache(),
   });
+
+export default client;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
 import client from './apolloClient';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
